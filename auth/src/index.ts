73a191f2de0f4c990d1b38e4e62c8d71547b9fe9
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-06-13 19:19:44 +0530

    Build Auth Service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,39 +1,7 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewears/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
+
+import { app } from "./app";
 import { DatabaseConnectionError } from "./errors/database-connection-error";
-
-const app = express();
-app.set("trust proxy", true);
-app.use(json());
-
-// Use  Cookie Session
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
